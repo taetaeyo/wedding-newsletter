d1beb881a63bfa4abecaadd70a42618cb0f9e59f
taetaeyo <57551024+taetaeyo@users.noreply.github.com>
2024-12-03 15:31:52 +0900

    이미지 용량 최적화

--- components/gallery.jsx
+++ components/gallery.jsx
@@ -4,24 +4,25 @@ import { Divider } from "antd";
 import styled from "styled-components";
 import "react-image-gallery/styles/css/image-gallery.css";
 
-const GalleryPhoto1 = "/assets/wedding/DSC00087.jpg";
-const GalleryPhoto2 = "/assets/wedding/DSC00418.jpg";
-const GalleryPhoto3 = "/assets/wedding/DSC00167.jpg";
-const GalleryPhoto4 = "/assets/wedding/DSC00616.jpg";
-const GalleryPhoto5 = "/assets/wedding/DSC00802-.jpg";
-const GalleryPhoto6 = "/assets/wedding/DSC00632.jpg";
-const GalleryPhoto7 = "/assets/wedding/DSC00369.jpg";
-const GalleryPhoto8 = "/assets/wedding/DSC00645.jpg";
-const GalleryPhoto9 = "/assets/wedding/DSC00861-.jpg";
-const GalleryPhoto10 = "/assets/wedding/DSC00510.jpg";
-const GalleryPhoto11 = "/assets/wedding/DSC00453.jpg";
-const GalleryPhoto12 = "/assets/wedding/DSC00238-.jpg";
-const GalleryPhoto13 = "/assets/wedding/DSC00848.jpg";
-const GalleryPhoto14 = "/assets/wedding/DSC00680.jpg";
-const GalleryPhoto15 = "/assets/wedding/DSC00444.jpg";
-const GalleryPhoto16 = "/assets/wedding/DSC00568-.jpg";
-const GalleryPhoto17 = "/assets/wedding/DSC00744.jpg";
-const GalleryPhoto18 = "/assets/wedding/DSC01038.jpg";
+// WebP로 변환된 최적화된 이미지 경로
+const GalleryPhoto1 = "/assets/wedding/DSC00087.webp";
+const GalleryPhoto2 = "/assets/wedding/DSC00418.webp";
+const GalleryPhoto3 = "/assets/wedding/DSC00167.webp";
+const GalleryPhoto4 = "/assets/wedding/DSC00616.webp";
+const GalleryPhoto5 = "/assets/wedding/DSC00802-.webp";
+const GalleryPhoto6 = "/assets/wedding/DSC00632.webp";
+const GalleryPhoto7 = "/assets/wedding/DSC00369.webp";
+const GalleryPhoto8 = "/assets/wedding/DSC00645.webp";
+const GalleryPhoto9 = "/assets/wedding/DSC00861-.webp";
+const GalleryPhoto10 = "/assets/wedding/DSC00510.webp";
+const GalleryPhoto11 = "/assets/wedding/DSC00453.webp";
+const GalleryPhoto12 = "/assets/wedding/DSC00238-.webp";
+const GalleryPhoto13 = "/assets/wedding/DSC00848.webp";
+const GalleryPhoto14 = "/assets/wedding/DSC00680.webp";
+const GalleryPhoto15 = "/assets/wedding/DSC00444.webp";
+const GalleryPhoto16 = "/assets/wedding/DSC00568-.webp";
+const GalleryPhoto17 = "/assets/wedding/DSC00744.webp";
+const GalleryPhoto18 = "/assets/wedding/DSC01038.webp";
 
 const Wrapper = styled.div`
   padding-top: 42px;
@@ -38,12 +39,12 @@ const Title = styled.p`
   text-align: center;
 `;
 
+// 이미지 데이터 생성
 const images = Array.from({ length: 18 }, (_, index) => {
   const id = index + 1;
   return {
     id,
     original: eval(`GalleryPhoto${id}`),
-    thumbnail: eval(`GalleryPhoto${id}`),
   };
 });
 
@@ -54,8 +55,9 @@ const Gallery = () => {
         <Title>우리의 아름다운 순간</Title>
       </Divider>
       <ImageGallery
-        showPlayButton={false}
-        showFullscreenButton={false}
+        showPlayButton={false} // 재생 버튼 비활성화
+        showFullscreenButton={false} // 전체 화면 버튼 비활성화
+        lazyLoad // Lazy Loading 활성화
         items={images}
       />
     </Wrapper>
