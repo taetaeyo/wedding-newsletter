aa8e7a6d9b1f6fc9e08ce9171c09d1ab4524e51e
taetaeyo <57551024+taetaeyo@users.noreply.github.com>
2024-12-05 18:05:26 +0900

    [add] OG

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,13 @@ import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     const siteUrl = `https://wedding-newsletter.vercel.app`; // 실제 사이트 URL로 변경하세요
-    const defaultOgImage = `${siteUrl}/assets/og-image.jpg`; // 이미지 경로를 실제 경로로 수정하세요
+    const defaultOgImage = `${siteUrl}/assets/wedding/DSC00234-.webp`; // 이미지 경로를 실제 경로로 수정하세요
 
     return (
         <>
             <Head>
                 {/* Open Graph 기본 메타 태그 */}
-                <meta property="og:title" content="WEDDING LETTER" />
+                <meta property="og:title" content="최태권 ♥ 임현정 결혼 소식" />
                 {/* <meta property="og:description" content="WEDDING LETTER" /> */}
                 <meta property="og:image" content={defaultOgImage} />
                 <meta property="og:url" content={siteUrl} />
